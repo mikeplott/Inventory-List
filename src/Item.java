9774ad069f6a2387106561b57497548f8cd02e3e
michaelplott <mike.plott80@gmail.com>
2016-09-19 14:42:18 -0400

    Completed inventory list assignment

---
+++ src/Item.java
@@ -0,0 +1,13 @@
+/**
+ * Created by michaelplott on 9/19/16.
+ */
+public class Item {
+
+    String itemName;
+    int itemQuantity;
+
+    public Item(String itemName, int itemQuantity) {
+        this.itemName = itemName;
+        this.itemQuantity = itemQuantity;
+    }
+}
---
+++ src/User.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+/**
+ * Created by michaelplott on 9/19/16.
+ */
+public class User {
+
+    String name;
+    String password;
+    ArrayList<Item> items;
+}
